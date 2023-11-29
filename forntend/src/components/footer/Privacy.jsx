890551b8f26390c36d015eef00e75cc9a5cfc3c0
NehaSMehta05 <nehasmehta2005.com>
2023-11-29 14:31:37 +0100

    created routing.jsx

---
+++ forntend/src/components/footer/Privacy.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Privacy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy for TarotFortuneTeller</h1>
+      <p>
+        Welcome to TarotFortuneTeller! We are committed to protecting your privacy and handling your personal information responsibly and securely.
+      </p>
+      <h2>Information We Collect</h2>
+      <p>
+        At TarotFortuneTeller, we may collect personal information, such as your name, email address, or other details when you use our services or interact with our website. This information is used to provide and improve our services and is not shared with third parties without your consent.
+      </p>
+      <h2>Data Security</h2>
+      <p>
+        We take stringent measures to ensure the security of your data. Your information is protected against unauthorized access, alteration, disclosure, or destruction.
+      </p>
+      
+    </div>
+  );
+};
+
+export default Privacy;
---
+++ forntend/src/components/footer/Terms.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Terms = () => {
+  return (
+    <div>
+      <h2>Terms and Conditions</h2>
+      <p>Welcome to TarotFortuneTeller! These terms and conditions outline the rules and regulations for the use of our website.</p>
+      <h3>1. Acceptance of Terms</h3>
+      <p>By accessing this website, we assume you accept these terms and conditions. Do not continue to use TarotFortuneTeller if you do not agree to all the terms and conditions stated on this page.</p>
+      <h3>2. Use License</h3>
+      <p>Permission is granted to temporarily download one copy of the materials (information or software) on TarotFortuneTeller's website for personal, non-commercial transitory viewing only.</p>
+      <h3>3. Limitations</h3>
+      <p>Under no circumstances shall TarotFortuneTeller or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on TarotFortuneTeller's website.</p>
+      <h3>4. Revisions and Errata</h3>
+      <p>The materials appearing on TarotFortuneTeller's website could include technical, typographical, or photographic errors.</p>
+     
+    </div>
+  );
+};
+
+export default Terms;
