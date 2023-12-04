ad5c81ee4cfb7d8c7715a42f87f8df83e30171cb
Masouma <masouma.rasouli@ild.edu.af>
2023-12-04 13:48:42 +0100

    masouma, reducer is added

--- forntend/src/context/Reducer.jsx
+++ forntend/src/context/Reducer.jsx
@@ -2,6 +2,10 @@ export const reducer = (state, action) => {
   switch (action.type) {
     case "setActiveTip":
       return { ...state, activeTip: action.payload };
+
+      case "setSubmitted":
+      return { ...state, submitted : action.payload };
+
     default:
       return state;
   }
@@ -9,4 +13,5 @@ export const reducer = (state, action) => {
 
 export const initialState = {
   activeTip: 0,
+  submitted: false,
 };
