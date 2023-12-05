d67a6ad7cffdce06b01847aee9ebcedbb79b0453
NehaSMehta05 <nehasmehta2005.com>
2023-12-05 11:22:32 +0100

    our team is created

--- forntend/src/components/mainSection/Routing.jsx
+++ forntend/src/components/mainSection/Routing.jsx
@@ -4,9 +4,9 @@ import AboutTarot from "../header/AboutTarot";
 import Terms from "../footer/Terms";
 import Privacy from "../footer/Privacy";
 import NotFound from "../notfoundPage/notfound";
-
 import ContactUs from "../header/ContactUs";
 import BuyCards from "../header/buyCards"
+import OurTeam from "../header/OurTeam";
 
 
 function Routing() {
@@ -15,8 +15,9 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutTarot" element={<AboutTarot />} />
-        <Route path="/yourFortuneTeller" />
+        <Route path="/yourfortuneteller" />
         <Route path="/buyCards" element={<BuyCards/>}/>
+        <Route path="/ourteam" element={<OurTeam />}/>
         <Route path="/contactUs" element={<ContactUs />}/>
         <Route path="/terms" element={<Terms />} />
         <Route path="/privacy" element={<Privacy />} />
